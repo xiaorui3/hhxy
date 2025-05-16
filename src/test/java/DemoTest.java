4cf145f09fc467a9a177303bee165825f41783d3
xiaorui <3505469466@qq.com>
2025-05-16 09:30:41 +0800

    test2

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,3 +1,4 @@
+import org.example.Demo;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,5 +7,10 @@ public class DemoTest {
 
     @Test
     public void js() {
+        Demo demo = new Demo();
+        float actaul = demo.js(2, 0, 4);
+        //System.out.println(js);
+        float expected =3L;
+
     }
 }
