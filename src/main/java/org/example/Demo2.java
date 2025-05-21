c4fa7f0cec4952e1454a5cd4f26b7ebe9fa58466
xiaorui <3505469466@qq.com>
2025-05-21 16:37:29 +0800

    test2

---
+++ src/main/java/org/example/Demo2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Demo2 {
+    public int  logicExemple(int x,int y){
+        int magic =0;
+        if(x>0&&y>0){
+            magic=x+y+10;
+        }else{
+            magic=x+y-10;
+        }
+        if (magic<0){
+            magic=0;
+        }
+        return magic;
+    }
+
+}
---
+++ src/test/java/org/example/Demo2Test.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Demo2Test {
+
+    @Test
+    public void logicExemple() {
+        Demo2 demo2 = new Demo2();
+        int a=demo2.logicExemple(1,1);
+        int e=12;
+        if (a==e){
+            System.out.println("<OK>");
+        }else{
+            System.out.println("<NOT OK>");
+        }
+    }
+}
